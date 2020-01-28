f44e6e194774b51d5d862f854cceb579738e4683
Diego <diballesterosc@gmail.com>
2020-01-28 17:40:52 -0500

    Caesar Cipher and array analysis testing

--- functions.js
+++ functions.js
@@ -1,5 +1,3 @@
-
-
 function capitalize(text) {
     return text.charAt(0).toUpperCase() + text.slice(1);
 };
@@ -23,8 +21,43 @@ var calculator = {
     }
 }
 
+function caesarCipher(text) {
+    var key = 3;
+    var lower = /[a-z]/;
+    var upper = /[A-Z]/;
+
+    var ciphertext = '';
+    for (i = 0; i < text.length; i++) {
+        if (lower.test(text.charAt(i))) {
+            ciphertext += String.fromCharCode((text.charCodeAt(i) - 97 + key) % 26 + 97);
+        } else if (upper.test(text.charAt(i))) {
+            ciphertext += String.fromCharCode((text.charCodeAt(i) - 65 + key) % 26 + 65);
+        }
+        else ciphertext += text.charAt(i);
+    } 
+    return ciphertext;
+}
+
+function arrayAnalysis(numbers) {
+    var stats = {};
+
+    stats.length = numbers.length;
+
+    stats.min = Math.min.apply(null, numbers);
+
+    stats.max = Math.max.apply(null, numbers);
+
+    stats.average = numbers.reduce((total, currentValue) => {
+        return total + currentValue;
+    }) / numbers.length;
+
+    return stats
+}
+
 module.exports = {
     calculator: calculator,
     capitalize: capitalize,
-    reverseString: reverseString
+    reverseString: reverseString,
+    caesarCipher: caesarCipher,
+    arrayAnalysis: arrayAnalysis
 };
--- functions.test.js
+++ functions.test.js
@@ -23,3 +23,19 @@ test('calculator division', () => {
 test('calculator multiplication', () => {
     expect(myFunctions.calculator.multiply(2, 2)).toBe(4);
 });
+
+test('Caesar Cipher encryption', () => {
+    expect(myFunctions.caesarCipher('abcdefghijklmnopqrstuvwxyz')).toBe('defghijklmnopqrstuvwxyzabc');
+});
+
+test('Caser Cipher punctuation', () => {
+    expect(myFunctions.caesarCipher(',.')).toBe(',.');
+});
+
+test('Caser Cipher case sensitive', () => {
+    expect(myFunctions.caesarCipher('ABCDEFGHIJKLMNOPQRSTUVWXYZ')).toBe('DEFGHIJKLMNOPQRSTUVWXYZABC');
+});
+
+test('Array analysis', () => {
+    expect(myFunctions.arrayAnalysis([1,1,2,4])).toEqual({average: 2, min: 1, max: 4, length: 4});
+});
